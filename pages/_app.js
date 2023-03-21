ccd17e45e23310428dae8fcaad54e96115787f4c
Enrico Weißenrieder <e.weissenrieder94@gmail.com>
2023-03-21 12:10:20 +0100

    implemented functionality and test for User Story 3

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import GlobalStyle from "../styles";
+import Layout from "../src/components/Layout/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <Layout>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
---
+++ pages/favorites.js
@@ -0,0 +1,7 @@
+export default function Favorites() {
+  return (
+    <div>
+      <h1>test fav page</h1>
+    </div>
+  );
+}
