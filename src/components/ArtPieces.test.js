8422b7e0eb7295bfa44b611b04b931a39613dfe4
Josh <joshuastolle445@gmail.com>
2023-03-21 16:15:48 +0100

    add component testing for ArtPieces

--- src/components/ArtPieces.test.js
+++ src/components/ArtPieces.test.js
@@ -1,18 +1,38 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import ArtPieces from "./ArtPieces";
+import Image from "next/image";
 
 const data = [
   {
-    title: "testValue",
+    name: "testValue",
     artist: "testVal",
-    image:
+    imageSource:
       "https://example-apis.vercel.app/assets/art/orange-red-and-green.jpg",
   },
 ];
 
-test("are these artpieces rendered in that list?", () => {
+test("are the artpieces rendered in a list?", () => {
   render(<ArtPieces pieces={data} />);
   const list = screen.getByRole("list");
   expect(list).toBeInTheDocument();
+});
+
+test("is image of artpieces rendered?", () => {
+  render(<ArtPieces pieces={data} />);
+  const img = screen.getByRole("img");
+  expect(img).toBeInTheDocument();
+  expect(img).toHaveAttribute("src", data.imageSource);
+});
+
+test("is title of artpieces rendered?", () => {
+  render(<ArtPieces pieces={data} />);
+  const heading = screen.getByRole("heading", { name: /testvalue/i });
+  expect(heading).toBeInTheDocument();
+});
+
+test("is artist of artpieces rendered?", () => {
+  render(<ArtPieces pieces={data} />);
+  const figcaption = screen.getByText("testVal");
+  expect(figcaption).toBeInTheDocument();
 });
