1b6148b76bb09395de725dd42f2ab07b14f898a0
Enrico Weißenrieder <e.weissenrieder94@gmail.com>
2023-03-22 19:42:52 +0100

    added functionality to fav page

---
+++ src/components/StyledList.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default StyledList;
